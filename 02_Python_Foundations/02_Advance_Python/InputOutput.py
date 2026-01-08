df351b03186a37a3d4f4e193d94f84dece7851a4
Partha Paul <parthapaul2705@gmail.com>
2026-01-08 11:49:31 +0530

    implement input & output

---
+++ 02_Python_Foundations/02_Advance_Python/InputOutput.py
@@ -0,0 +1,15 @@
+name = "Partha"
+agee = 20
+print(name, agee)
+print("my name is " + name,"and my age is", agee)
+print(f"my name is {name} and my age is {agee}")
+
+agee = int(input("Hello what is your age !! \n"))
+print(agee)
+
+#Accept number from the user
+num = str(input("Enter your number :\n"))
+#get username from them
+Username = input("Enter your username !! \n")
+print(f"Hey {Username} !welcome thanks for conforming , Your age is {agee}")
+

1c6efd74e2c724886c95c1c1c3e03be8f8440ade
Partha Paul <parthapaul2705@gmail.com>
2026-01-08 10:01:06 +0530

    basic implement of type convertion

---
+++ 02_Python_Foundations/02_Advance_Python/typeConvertion.py
@@ -0,0 +1,21 @@
+a=12
+# print(type(a))
+# <class 'int'>
+
+a =str(a)
+print(type(a))
+#<class 'str'>
+
+
+st ="12344321"
+#print(type(st))
+#<class 'str'>
+st =int(st)
+print(type(st))
+#<class 'int'>
+# st =float(st) # for convert the type into float
+# st =bool(st) # for conver the type into boolean
+
+num =0
+print(bool(num))
+print(type(num))

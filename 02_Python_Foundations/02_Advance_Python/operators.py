ba362fb51477ba8013303229042ac2f5a1e729db
Partha Paul <parthapaul2705@gmail.com>
2026-01-09 12:09:51 +0530

    Complete exercises on Python operators

---
+++ 02_Python_Foundations/02_Advance_Python/operators.py
@@ -0,0 +1,45 @@
+print("welcome to Python programming Arithmetic operator  ")
+
+a=120
+b =20
+print (a+b)
+
+summation = a + b
+print (sum)
+
+print( a-b )
+deviation = a - b
+
+print(deviation)
+print(a*b)
+
+multiplication = a * b
+print(multiplication)
+
+print(a/b)
+print(a%b) # for find reminder
+print(a**b)
+
+
+
+print("Assignment operator\n")
+p =99
+q = 88
+p+=q
+print(p)
+
+print("Comparison operator\n")
+print("comparison operator work with number but also work with string \n")
+#  comparison operator always provide boolean result
+
+
+m=22
+n=22
+print (m>n)
+print(m<n)
+print(m>=n)
+print(m==n)
+print (m!=n)
+print(m<=n)
+
+print("logical operators ")

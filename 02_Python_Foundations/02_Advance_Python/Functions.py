16c9dbfe9373d225ce49e6f92b0f3ec0a7705e03
Partha Paul <parthapaul2705@gmail.com>
2026-01-10 14:54:38 +0530

    add parameter argument in this program

--- 02_Python_Foundations/02_Advance_Python/Functions.py
+++ 02_Python_Foundations/02_Advance_Python/Functions.py
@@ -2,3 +2,11 @@ def demo():
     for i in range(100):
         print("Hello World")
 demo()
+# this is parameter argument
+def sum(a,b):
+    print(f"The sum of the number is {a + b}")
+
+sum(12,45)
+sum(45,55)
+sum(33,55)
+

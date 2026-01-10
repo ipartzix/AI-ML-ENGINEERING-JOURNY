3fb50479e36631d93cae0985b1cd69a2a87bec8b
Partha Paul <parthapaul2705@gmail.com>
2026-01-10 21:17:18 +0530

    List traversal two type methods  implemented

--- 02_Python_Foundations/02_Advance_Python/ListDataStructure.py
+++ 02_Python_Foundations/02_Advance_Python/ListDataStructure.py
@@ -3,3 +3,11 @@ number =[11,55,3434, 55]
 print(number[3])
 print(fruits[2])
 print(number[0::1])
+
+# 1st way of list Traversal using index
+for i in range(len(number)):
+    print(number[i])
+
+# 2nd way of list Traversal Directly on value
+for i in number:
+    print(i)

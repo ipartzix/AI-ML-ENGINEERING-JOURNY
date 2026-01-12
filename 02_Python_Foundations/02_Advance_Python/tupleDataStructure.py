91903b1f9e175d22900b18fcb1070a6fc95bf59a
Partha Paul <parthapaul2705@gmail.com>
2026-01-12 14:14:42 +0530

    Implement tuple unpacking

--- 02_Python_Foundations/02_Advance_Python/tupleDataStructure.py
+++ 02_Python_Foundations/02_Advance_Python/tupleDataStructure.py
@@ -11,3 +11,23 @@ print(index)
 print("count of occurrences ")
 count = a.count(5)
 print(count)
+
+print("Tuple unpacking ")
+a, b, c, d, e, f, g, h = (1, 2, 3, 4, 5, 6, 7, 8)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+j = (86)  # in tuple single value are always unpack
+print(j)
+print(type(j))
+# <class 'int'>
+
+m = (33,)  # ( ,) coma will help to blocking unpacking the tuple
+print(type(m))
+# <class 'tuple'>

530ba60df175084840290a02f6c21c247c1cbf9f
Partha Paul <parthapaul2705@gmail.com>
2026-01-15 20:56:02 +0530

    Creating an object of the class

--- 02_Python_Foundations/02_Advance_Python/ATM_OOPs.py
+++ 02_Python_Foundations/02_Advance_Python/ATM_OOPs.py
@@ -10,19 +10,23 @@ class Atm:
     def menu(self):
         user_input = input(""" 
                 Hello , how would like to proceed ?
-                1.Enter 1 to create pin .
+                1.Enter 1 to create pin 
                 2.Enter 2 to deposit 
                 3.Enter 3 to withdraw 
                 4.Enter 4 to check balance 
                 5.Enter 5 to exit
         """)
-        if user_input == 1:
+        if user_input == "1":  # Fixed: comparing string to string
             print("Create pin! ")
-        elif user_input == 2:
+        elif user_input == "2":
             print("Deposit pin! ")
-        elif user_input == 3:
+        elif user_input == "3":
             print("Withdraw pin! ")
-        elif user_input == 4:
+        elif user_input == "4":
             print("Check balance! ")
         else:
             print("Bye ")
+
+
+# Creating an object of the class
+sbi = Atm()

cc250952a39719247391c6e8f564e0369fe812d9
Partha Paul <parthapaul2705@gmail.com>
2026-01-15 20:39:37 +0530

    implement self

--- 02_Python_Foundations/02_Advance_Python/ATM_OOPs.py
+++ 02_Python_Foundations/02_Advance_Python/ATM_OOPs.py
@@ -1,7 +1,7 @@
 # It is a full flared ATM machine system design in oops code structure
 # to understand the concept of Object-Oriented-Programming
 class Atm:
-    def __init__(self):  # __init__ is a constructor // Constructor is a special method
+    def __init__(self):  # __init__ is a constructor // Constructor is a special method  ,< self > always run itself
         self.pin = ""
         self.balance = 0
 

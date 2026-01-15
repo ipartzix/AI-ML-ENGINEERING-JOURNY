bc8c91adbb10d2f2f5f87af89607a39ac756027f
Partha Paul <parthapaul2705@gmail.com>
2026-01-15 17:31:21 +0530

    make privious code as comment line

--- 02_Python_Foundations/02_Advance_Python/OOPSPython.py
+++ 02_Python_Foundations/02_Advance_Python/OOPSPython.py
@@ -1,13 +1,13 @@
-class Factory:
-    a = 12  # class attribute
-
-    def __init__(self):
-        print("this is an initialized statement")
-
-    def hello(self):  # instance method
-        print("hello how are you")
-
-
-obj = Factory()  # calls __init__
-obj.hello()  # calls method
-print(obj.a)  # calls class attribute
+# class Factory:
+#     a = 12  # class attribute
+#
+#     def __init__(self):
+#         print("this is an initialized statement")
+#
+#     def hello(self):  # instance method
+#         print("hello how are you")
+#
+#
+# obj = Factory()  # calls __init__
+# obj.hello()  # calls method
+# print(obj.a)  # calls class attribute

0438885c39cfb45e86842fa98b375dfdff1f7c77
Partha Paul <parthapaul2705@gmail.com>
2026-01-15 20:48:39 +0530

    implement menu list

--- 02_Python_Foundations/02_Advance_Python/ATM_OOPs.py
+++ 02_Python_Foundations/02_Advance_Python/ATM_OOPs.py
@@ -8,4 +8,11 @@ class Atm:
         self.menu()
 
     def menu(self):
-        pass
+        user_input = input(""" 
+                Hello , how would like to proceed ?
+                1.Enter 1 to create pin .
+                2.Enter 2 to deposit 
+                3.Enter 3 to withdraw 
+                4.Enter 4 to check balance 
+                5.Enter 5 to exit
+        """)

f728e7d5c66b7480e4b879a174314ac88863f17b
Partha Paul <parthapaul2705@gmail.com>
2026-01-15 18:58:48 +0530

    class implement in oops

--- 02_Python_Foundations/02_Advance_Python/OOPSPython.py
+++ 02_Python_Foundations/02_Advance_Python/OOPSPython.py
@@ -11,3 +11,34 @@
 # obj = Factory()  # calls __init__
 # obj.hello()  # calls method
 # print(obj.a)  # calls class attribute
+
+"""
+      The core concept of oops are
+      1. Object
+      2. Class
+      3. Abstraction
+      4. Polymorphism
+      5. Ecapsulation
+      6. Inheritances
+
+
+
+*CLASS*
+
+class is a blueprint --> (how an object is behave )
+"""
+
+
+class Car:  # always make it capital letter (PascalCase)
+    colour = "Blue"  # data
+    model = "Sports"  # data
+
+    def calculate_avg_speed(self, km, time):  # snake_case
+        avg = km / time
+        print("The average speed is", avg)
+
+
+my_car = Car()
+
+# Pass the values for km and time here
+my_car.calculate_avg_speed(120, 2)

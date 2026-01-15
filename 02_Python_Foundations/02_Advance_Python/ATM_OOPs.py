2728d96be024882d0ab723061c57f2b0161b8b1a
Partha Paul <parthapaul2705@gmail.com>
2026-01-15 20:25:11 +0530

    Introduce with constructor

---
+++ 02_Python_Foundations/02_Advance_Python/ATM_OOPs.py
@@ -0,0 +1,11 @@
+# It is a full flared ATM machine system design in oops code structure
+# to understand the concept of Object-Oriented-Programming
+class Atm:
+    def __init__(self):  # __init__ is a constructor // Constructor is a special method
+        self.pin = ""
+        self.balance = 0
+
+        self.menu()
+
+    def menu(self):
+        pass

9cd7987cc95f30db0c353616264a29bfc7e547e1
Partha Paul <parthapaul2705@gmail.com>
2026-01-16 19:13:14 +0530

    add comment line for easy explanation and future references

--- 02_Python_Foundations/02_Advance_Python/FractionOOPs.py
+++ 02_Python_Foundations/02_Advance_Python/FractionOOPs.py
@@ -1,33 +1,53 @@
-# Creating own data type
-
+# Creating our own data type using a class
 class Fraction:
-    def __init__(self, n, d):
-        self.num = n  # Nenominator
-        self.den = d  # Denominator
 
+    # Constructor: runs automatically when an object is created
+    def __init__(self, n, d):
+        self.num = n  # numerator (top part of fraction)
+        self.den = d  # denominator (bottom part of fraction)
+
+    # This method controls what is printed when we print the object
     def __str__(self):
+        # Converts the fraction object into a string like "3/4"
         return "{}/{}".format(self.num, self.den)
 
+    # Overloading the + operator
     def __add__(self, other):
+        # Formula: a/b + c/d = (ad + bc) / bd
         temp_num = self.num * other.den + other.num * self.den
         temp_den = self.den * other.den
+
+        # Returning result as a string (not as a Fraction object)
         return "{}/{}".format(temp_num, temp_den)
 
+    # Overloading the - operator
     def __sub__(self, other):
+        # Formula: a/b - c/d = (ad - bc) / bd
         temp_num = self.num * other.den - other.num * self.den
         temp_den = self.den * other.den
+
+        # Returning result as a string
         return "{}/{}".format(temp_num, temp_den)
 
+    # Overloading the * operator
     def __mul__(self, other):
+        # Multiplying numerator with denominator (logical mistake, but kept as-is)
         temp_num = self.num * other.den
         temp_den = self.den * other.den
+
+        # Returning result as a string
         return "{}/{}".format(temp_num, temp_den)
 
 
+# Creating Fraction objects
 x = Fraction(3, 4)
 y = Fraction(5, 6)
-print(x)
-print(y)
-print(x + y)
-print(x - y)
-print(x * y)
+
+# Printing the fractions
+print(x)  # Calls __str__ → "3/4"
+print(y)  # Calls __str__ → "5/6"
+
+# Performing operations using operator overloading
+print(x + y)  # Calls __add__
+print(x - y)  # Calls __sub__
+print(x * y)  # Calls __mul__

33b0dbf0cdb4794df1b4c975eb7597fea1b1683d
Partha Paul <parthapaul2705@gmail.com>
2026-01-17 20:18:31 +0530

    implement a basic greet and also add gender

--- 02_Python_Foundations/02_Advance_Python/PassByReference.py
+++ 02_Python_Foundations/02_Advance_Python/PassByReference.py
@@ -1,13 +1,19 @@
 class Customer:
-    def __init__(self, name):
+    def __init__(self, name, gender):
         self.name = name
+        self.gender = gender
+
 
 
 def greet(customer):
-    print("Hello", customer.name)
+    if customer.gender == "Male":
+        print("Hello", customer.name, "sir")
+
+    else:
+        print("Hello", customer.name, "ma'am")
 
 
-cust = Customer("Partha")
+cust = Customer("Partha", "Male")
 print(cust.name)
 
 greet(cust)

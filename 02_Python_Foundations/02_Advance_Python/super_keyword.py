eba1c9d4431b0036aa843a01c669eafa15f64e5f
Partha Paul <parthapaul2705@gmail.com>
2026-01-18 16:30:07 +0530

    Implement supper keyword key concept with example
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/02_Advance_Python/super_keyword.py
+++ 02_Python_Foundations/02_Advance_Python/super_keyword.py
@@ -16,6 +16,39 @@ class SmartPhone(Phone):
 
 
 """
+Using super keyword we can access only 2 thing from parent class to child class
+access list:-
+               Parent method
+               Parent constructor 
+access not possible :-
+               Parent  attributes   
+               
+Parent class
+______________
+  class Phone:
+    def __init__(self, price):
+        self.__price = price    # private attribute
+
+    def show_price(self):
+        print("Price:", self.__price)
+        
+        
+Child class
+____________
+  class SmartPhone(Phone):
+    def __init__(self, price):
+        super().__init__(price)   # ✅ Parent constructor
+
+    def buy(self):
+        super().show_price()      # ✅ Parent method
+
+
+
+❌ Access NOT possible (parent attribute)
+____________________________________________
+class SmartPhone(Phone):
+    def get_price(self):
+        print(super().__price)   # ❌ ERROR
 
 """
 

f5d667769df1615fdbc42c742a913759fb448272
Partha Paul <parthapaul2705@gmail.com>
2026-01-20 08:09:39 +0530

    update book.py folder and categories display categories , single book object
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 10_End_to_End_Projects/Library_Management_System/src/book.py
+++ 10_End_to_End_Projects/Library_Management_System/src/book.py
@@ -1,3 +1,24 @@
-class BookID:
-    def __init__(self):
-        print("hii")
+from books_data import BOOKS_BY_CATEGORY
+
+
+class Book:
+    def __init__(self, book_id, title, author):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+
+    # ______________________display a single book object_______________________
+
+    def display(self):
+        print(f"[{self.book_id}] {self.title} - {self.author}")
+
+    # _______________________Display book categories___________________________
+
+    def show_categories(self):
+        print("\n_______ Book Categories _______")
+        for key, value in BOOKS_BY_CATEGORY.items():
+            print(f"{key}. {value['name']}")
+
+
+book = Book("CS101", "Introduction to Algorithms", "Cormen")
+book.display()

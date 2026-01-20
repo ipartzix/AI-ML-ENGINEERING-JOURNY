9bf469d290ab22cfe35a7de5aae66847b9531dd7
Partha Paul <parthapaul2705@gmail.com>
2026-01-20 09:45:05 +0530

    implement user select method
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 10_End_to_End_Projects/Library_Management_System/src/library.py
+++ 10_End_to_End_Projects/Library_Management_System/src/library.py
@@ -1,3 +1,4 @@
+from book import Book
 from books_data import BOOKS_BY_CATEGORY
 
 
@@ -11,7 +12,22 @@ class Library:
     def show_categories(self):
         print("\n_______ Book Categories _______")
         for key, value in BOOKS_BY_CATEGORY.items():
-            print(f"{key}. {value['name']}")
+            print(f"key:-{key}. {value['name']}")
+
+    # _____________________ user select single domain_______________________
+    def userBook(self):
+        choice = input("\nEnter category key: ")
+        category = BOOKS_BY_CATEGORY.get(choice)
+
+        if not category:
+            print("Invalid category")
+            return None
+
+        print(f"\n{category['name']}")
+        for data in category["books"]:
+            Book(data["id"], data["title"], data["author"]).display()
+
+        return category
 
     def bookIssue(self):
         print("book issue done")
@@ -30,4 +46,5 @@ class Library:
 
 
 book = Library()
-book.bookIssue()
+book.show_categories()
+book.userBook()

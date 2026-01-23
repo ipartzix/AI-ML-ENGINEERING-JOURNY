14a59d025c75ebc9c9e990a52db60f5be0edb201
Partha Paul <parthapaul2705@gmail.com>
2026-01-23 19:40:25 +0530

    Implement timeit magic function logic
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/Numpy/Introduction_to_numPy.py
+++ 02_Python_Foundations/Numpy/Introduction_to_numPy.py
@@ -8,3 +8,6 @@ print(x)
 print(lis_t)
 print(type(x))  # <class 'numpy.ndarray'>
 print(type(lis_t))  # <class 'list'>
+
+# %timeit [j**4 for j in range(1,9)] # it use to se time to execution for single line
+# %%timeit [j**4 for j in range(1,9)] # it is use to see full code execution time

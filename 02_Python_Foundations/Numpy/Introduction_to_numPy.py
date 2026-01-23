83c71a7cbb51e168d055409dcc689d96f0ad2904
Partha Paul <parthapaul2705@gmail.com>
2026-01-23 19:16:56 +0530

    Implement array and list
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/Numpy/Introduction_to_numPy.py
+++ 02_Python_Foundations/Numpy/Introduction_to_numPy.py
@@ -1,2 +1,10 @@
-print("hii")
-print(1 + 5)
+import numpy as np
+
+print("Numpy Learning ")
+
+lis_t = [1, 2, 3, 4]
+x = np.array([1, 2, 3, 4])
+print(x)
+print(lis_t)
+print(type(x))  # <class 'numpy.ndarray'>
+print(type(lis_t))  # <class 'list'>

65f1fdc25e68cf5a5952584787de32ce8d9fc0d9
Partha Paul <parthapaul2705@gmail.com>
2026-01-23 19:47:15 +0530

    NumPy as np
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/Numpy/Introduction_to_numPy.py
+++ 02_Python_Foundations/Numpy/Introduction_to_numPy.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+# Standard alias for Numerical Python; used for high-performance vector/matrix operations
+# and as the foundation for most AI-ML libraries (Scikit-learn, TensorFlow, etc.).
+
 print("Numpy Learning ")
 
 lis_t = [1, 2, 3, 4]
@@ -11,3 +14,5 @@ print(type(lis_t))  # <class 'list'>
 
 # %timeit [j**4 for j in range(1,9)] # it use to se time to execution for single line
 # %%timeit [j**4 for j in range(1,9)] # it is use to see full code execution time
+
+print("For create an NumPy array we use the function np.array()")

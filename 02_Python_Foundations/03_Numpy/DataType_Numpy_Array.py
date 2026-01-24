6501cb9c0fb9c2f8a04a9883f6f8341ff8f74b9c
Partha Paul <parthapaul2705@gmail.com>
2026-01-24 21:08:39 +0530

    feat(datatype numpy): add note about datatype in NumPy
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

---
+++ 02_Python_Foundations/03_Numpy/DataType_Numpy_Array.py
@@ -0,0 +1,36 @@
+# Integer Types
+# | Type   | Description             |
+# | -------|-------------------------|
+# | int8   | 8-bit integer           |
+# | int16  | 16-bit integer          |
+# | int32  | 32-bit integer          |
+# | int64  | 64-bit integer          |
+# | uint8  | 8-bit unsigned integer  |
+# | uint16 | 16-bit unsigned integer |
+# | uint32 | 32-bit unsigned integer |
+# | uint64 | 64-bit unsigned integer |
+# NumPy Array Data Types (dtype)
+
+# Integer types:
+# int8, int16, int32, int64
+# uint8, uint16, uint32, uint64
+
+# Floating-point types:
+# float16, float32, float64
+
+# Complex number types:
+# complex64, complex128
+
+# Boolean type:
+# bool_
+
+# String types:
+# U  (Unicode string)
+# S  (Byte string)
+
+# Object type:
+# object_
+
+# Date and time types:
+# datetime64
+# timedelta64

0dac1fbf5f83df26c19fef8cdd8c58b09c09885f
Partha Paul <parthapaul2705@gmail.com>
2026-01-24 11:37:08 +0530

    feat(numpy): add examples for one-filled array creation
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/Numpy/Special_Types_of_Array.py
+++ 02_Python_Foundations/Numpy/Special_Types_of_Array.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-print("_________________________________________________________________________")
 print("Special type of Array")
+print("_________________________________________________________________________")
 print("Array filled with zero")
 ar_zero = np.zeros(4)  # zeros is a function which help to create Array filled with zero
 ar_zero1 = np.zeros((4, 2))
 print(ar_zero)
 print("\n4,2 array")
 print(ar_zero1)
+
+print("__________________________________________________________________________")
+print("Array filled with 1")
+arr_one = np.ones(3)
+print(arr_one)

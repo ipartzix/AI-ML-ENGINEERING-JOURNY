ce80f66e48d05ae30fe3b49c0d34c95bed736beb
Partha Paul <parthapaul2705@gmail.com>
2026-01-24 11:43:13 +0530

    feat(numpy): add examples for empty array creation
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/Numpy/Special_Types_of_Array.py
+++ 02_Python_Foundations/Numpy/Special_Types_of_Array.py
@@ -11,5 +11,10 @@ print(ar_zero1)
 
 print("__________________________________________________________________________")
 print("Array filled with 1")
-arr_one = np.ones(3)
+arr_one = np.ones(3)  # ones is the keyword to do this kind of operation
 print(arr_one)
+
+print("__________________________________________________________________________")
+print("Empty array ")
+ar_empty = np.empty(4)  # .empty is the keyword to do that kind of operation
+print(ar_empty)

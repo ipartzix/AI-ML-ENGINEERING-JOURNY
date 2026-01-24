c51ba27b6e738284f94a40fb35cfe7840823f858
Partha Paul <parthapaul2705@gmail.com>
2026-01-24 18:51:07 +0530

    feat(numpy): add random example for 1D ,2D etc. array example
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

---
+++ 02_Python_Foundations/03_Numpy/NumPy_Array_with_Random_Numbers.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# rand() = the function is used to generate a random value between 0 to 1 .
+print("For creating 1D arr")
+arr = np.random.rand(3)
+
+print(arr)
+print()
+arr1 = np.random.rand(2, 4)
+print(arr1)
+
+arr3 = np.random.rand(2, 4, 5)
+print(arr3)

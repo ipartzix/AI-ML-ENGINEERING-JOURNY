3b858f5b2f66ec96aacf7df596bf9ba5280b318d
Partha Paul <parthapaul2705@gmail.com>
2026-01-25 13:31:22 +0530

    feat(datatype numpy): add note about arithmetic operation in basic method in NumPy
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

---
+++ 02_Python_Foundations/03_Numpy/Arithmetic_Operatin_NumPy.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+print("Arithmetic Operation ")
+
+print("normal method like + , - , *, /, %")
+
+var1 = np.array([1, 2, 3, 4])
+var2 = np.array([9, 8, 7, 6])
+
+print("add")
+var_add = var1 + var2
+print(var_add)
+
+print("sub")
+varsub = var1 - var2
+print(varsub)
+
+print("mul")
+varmal = var1 * var2
+print(varmal)
+
+print("div")
+vardiv = var1 / var2
+print(vardiv)
+
+print("modulo")
+varmod = var1 % var2
+print(varmod)

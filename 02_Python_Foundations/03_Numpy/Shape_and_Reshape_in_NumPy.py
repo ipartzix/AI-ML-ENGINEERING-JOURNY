1099f89458f16ea99709df9cf5f907fe5b479e17
Partha Paul <parthapaul2705@gmail.com>
2026-01-25 13:05:27 +0530

    feat(datatype numpy): add note about reshape 1D to 3D and 3D to 1D in NumPy
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Shape_and_Reshape_in_NumPy.py
+++ 02_Python_Foundations/03_Numpy/Shape_and_Reshape_in_NumPy.py
@@ -16,3 +16,8 @@ print("RESHAPE")
 var2 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 mx = var2.reshape(3, 3)  # 1st is number of rows and 2nd is number of column
 print(mx)
+
+print()
+one = mx.reshape(-1)
+print(one)
+print(one.ndim)

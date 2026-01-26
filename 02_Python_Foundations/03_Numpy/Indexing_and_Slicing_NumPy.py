d2f5e88171919bad2f6b83c5404e9ad01164eaea
Partha Paul <parthapaul2705@gmail.com>
2026-01-26 16:18:44 +0530

    feat(numpy):Implement 2D array Indexing example
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Indexing_and_Slicing_NumPy.py
+++ 02_Python_Foundations/03_Numpy/Indexing_and_Slicing_NumPy.py
@@ -3,5 +3,12 @@ import numpy as np
 
 print("1D array Indexing ")
 x = np.array([1, 7, 56, 77, 54])
+print(x.ndim)
 print(x[1])
 print(x[-4])
+
+print("2D array Indexing ")
+y = np.array([[1, 2, 3, 4], [3, 4, 5, 6]])
+print(y.ndim)
+print(y[0, 2])
+print(y[0, -1])

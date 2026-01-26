a2a2c2179038f59af1d2b780d54280be29e545a8
Partha Paul <parthapaul2705@gmail.com>
2026-01-26 16:52:46 +0530

    feat(numpy):Implement 1D array slicing example
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Indexing_and_Slicing_NumPy.py
+++ 02_Python_Foundations/03_Numpy/Indexing_and_Slicing_NumPy.py
@@ -18,3 +18,8 @@ z = np.array([[[1, 2, 3], [55, 66, 8], [12, 4, 77]]])
 print(z.ndim)
 print(z)
 print(z[0, 1, 2])
+
+print("SLICING")
+s = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+# slicing process done with -------------> | start : stop : step |
+print(s[1:5])

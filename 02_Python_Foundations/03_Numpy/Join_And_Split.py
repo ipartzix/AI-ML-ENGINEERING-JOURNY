44ae42fd2f6f6c4a5245c6560e187d2602334870
Partha Paul <parthapaul2705@gmail.com>
2026-01-27 14:01:26 +0530

    feat(numpy):implement concatenate function with logic and example also note in 2D array
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Join_And_Split.py
+++ 02_Python_Foundations/03_Numpy/Join_And_Split.py
@@ -6,3 +6,10 @@ y = np.array([9, 8, 7, 6, 5, 4, 3, 2, 1])
 
 n = np.concatenate((x, y))
 print(n)
+
+print(" join of 2D array ")
+x1 = np.array([[1, 2, 3, 4], [5, 6, 7, 8, ]])
+y1 = np.array([[9, 8, 7, 6], [5, 4, 3, 2, ]])
+
+n2 = np.concatenate((x1, y1))
+print(n2)

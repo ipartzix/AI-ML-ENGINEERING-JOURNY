d7fede4ce7dd27704f1d2746af8cbf2f00acc59e
Partha Paul <parthapaul2705@gmail.com>
2026-01-27 13:34:25 +0530

    feat(numpy):implement view function with logic and example
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Copy_And_View_NumPy.py
+++ 02_Python_Foundations/03_Numpy/Copy_And_View_NumPy.py
@@ -6,3 +6,7 @@ print(f"Print the actual variable", x)
 print(f"Print the copy variable", co)
 print(".copy() in NumPy creates a new array with its own memory,"
       " independent of the original array.")
+vi = x.view()
+print(vi)
+print(".view() in NumPy creates a new array object"
+      " that shares the same data as the original array.")

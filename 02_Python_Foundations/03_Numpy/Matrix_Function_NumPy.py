e4283655766571daf5d3af5cc73039064555c27e
Partha Paul <parthapaul2705@gmail.com>
2026-01-28 17:53:56 +0530

    docs(numpy):implement Swapaxes matrix in numpy
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Matrix_Function_NumPy.py
+++ 02_Python_Foundations/03_Numpy/Matrix_Function_NumPy.py
@@ -1,8 +1,14 @@
 import numpy as np
 
+print("Transpose ")
 var = np.matrix([[1, 2, 3], [4, 5, 6]])
 print(var)
 print()
 print(np.transpose(var))
 print()
 print(var.T)  # T is a shortcut of this function transpose
+print("Swapaxes")
+var2 = np.matrix([[1, 2], [3, 4]])
+print(var2)
+print()
+print(np.swapaxes(var2, 0, 1))

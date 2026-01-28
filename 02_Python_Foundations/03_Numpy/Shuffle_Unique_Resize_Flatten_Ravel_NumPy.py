a4ff64eaf07935b7a0c0e1f26ef3a5a0e1eca132
Partha Paul <parthapaul2705@gmail.com>
2026-01-28 10:35:29 +0530

    docs(numpy):implement unique also some parameters  function with note and example
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Shuffle_Unique_Resize_Flatten_Ravel_NumPy.py
+++ 02_Python_Foundations/03_Numpy/Shuffle_Unique_Resize_Flatten_Ravel_NumPy.py
@@ -8,6 +8,8 @@ print("Shuffle don't have return type , it just return the original variable ")
 
 print("Unique")
 new_arr = np.array([1, 2, 3, 4, 5, 4, 3, 2])
-x = np.unique(new_arr)
+x = np.unique(new_arr, return_index=True, return_counts=True)
+# return_index= True is print the index value of unique element
+# return_counters =True it print the counter of it
 print(x)
 print("unique function print unique element that come once in array ")

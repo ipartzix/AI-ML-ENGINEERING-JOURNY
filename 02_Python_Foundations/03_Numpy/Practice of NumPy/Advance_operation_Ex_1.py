8cdb1b7d86a37e30a32426aa308f6df44d6c0022
Partha Paul <parthapaul2705@gmail.com>
2026-02-01 13:59:30 +0530

    feat(numpy):implement tMinimum sales per restaurant
    Signed-off-by: Partha Paul <parthapaul2705@gmail.com>

--- 02_Python_Foundations/03_Numpy/Practice of NumPy/Advance_operation_Ex_1.py
+++ 02_Python_Foundations/03_Numpy/Practice of NumPy/Advance_operation_Ex_1.py
@@ -17,3 +17,7 @@ print("Total sales per year ")
 print(np.sum(sales_data, axis=0))
 yearly_total = np.sum(sales_data[:, 1:], axis=0)
 print(yearly_total)
+
+# Minimum sales per restaurant
+min_sales = np.min(sales_data[:, 1:], axis=1)
+print(min_sales)

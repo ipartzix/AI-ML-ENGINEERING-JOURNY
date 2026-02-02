4e38841f5307f28c0451ffd331860abaed9b56f7
ipartzix <parthaPaul2705@gmail.com>
2026-02-02 20:25:29 +0530

    feat(pandas):Create sample dataset
    Signed-off-by: ipartzix <parthaPaul2705@gmail.com>

---
+++ 02_Python_Foundations/04_Pandas/pandas_data_cleaning_analysis.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+# Create sample dataset
+data = {
+    "Employee_ID": [1, 2, 3, 4, 5, 6],
+    "Name": ["Rahul", "Anita", "Vikas", "Pooja", "Suresh", "Kavya"],
+    "Department": ["IT", "HR", "IT", "Finance", "HR", "IT"],
+    "Salary": [55000, 48000, np.nan, 62000, 50000, np.nan],
+    "Experience_Years": [2, 3, 1, 5, 4, 2]
+}
+
+df = pd.DataFrame(data)
+
+print("Original Data:")
+print(df)

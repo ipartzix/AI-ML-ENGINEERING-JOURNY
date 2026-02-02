94bdf7c005df6d2e9c99bd986d5dcadc6306367b
ipartzix <parthaPaul2705@gmail.com>
2026-02-02 20:09:03 +0530

    feat(python):Create a sample DataFrame
    Signed-off-by: ipartzix <parthaPaul2705@gmail.com>

--- 02_Python_Foundations/04_Pandas/pandas_learn.py
+++ 02_Python_Foundations/04_Pandas/pandas_learn.py
@@ -1,3 +1,10 @@
 import pandas as pd
 
 print(pd.__version__)
+data = {
+    "Student_ID": [101, 102, 103, 104, 105],
+    "Name": ["Amit", "Riya", "Sohan", "Neha", "Karan"],
+    "Marks": [78, 85, 62, 90, 55],
+    "Branch": ["CSE", "CSE", "ECE", "CSE", "ME"]
+}
+df = pd.DataFrame(data)

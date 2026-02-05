2cdc4bdca863ac9401625a5ba06146a79feea1fa
ipartzix <parthaPaul2705@gmail.com>
2026-02-05 23:04:23 +0530

    feat(pandas):implement the concept of view data
    Signed-off-by: ipartzix <parthaPaul2705@gmail.com>

--- 02_Python_Foundations/04_Pandas/Basic_Pandas/pandas_basic_learn.py
+++ 02_Python_Foundations/04_Pandas/Basic_Pandas/pandas_basic_learn.py
@@ -46,10 +46,33 @@ Columns → Name, age, Marks
 
 Rows → Individual student records
 """
-print()
+print("-----------understanding the row ,coluimns and index-----------")
 print("index:---------")
 print(df.index)
 print("columns:-------")
 print(df.columns)
 print("shape----------")
 print(df.shape)
+
+print()
+print("------------------------view data-------------------------")
+
+print("---head---")
+print(df.head())
+
+print("---tail---")
+print(df.tail())
+
+print("---info---")
+df.info()
+print("---describe---")
+df.describe()
+"""
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 3 entries, 0 to 2
+Data columns (total 3 columns):
+ 0   Name    3 non-null object
+ 1   age     3 non-null int64
+ 2   Marks   3 non-null int64
+
+"""

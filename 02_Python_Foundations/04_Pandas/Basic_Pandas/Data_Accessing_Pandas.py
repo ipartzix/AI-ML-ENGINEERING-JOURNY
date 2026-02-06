d02a75a6725606333776b983fd650c6961258390
ipartzix <parthaPaul2705@gmail.com>
2026-02-06 12:48:59 +0530

    fix(pandas):implement Access Rows with example and note
    Signed-off-by: ipartzix <parthaPaul2705@gmail.com>

--- 02_Python_Foundations/04_Pandas/Basic_Pandas/Data_Accessing_Pandas.py
+++ 02_Python_Foundations/04_Pandas/Basic_Pandas/Data_Accessing_Pandas.py
@@ -10,5 +10,17 @@ print(df)
 
 print("----For Access Columns----")
 print(df["Name"])
+print("----------------------------------")
 print(df[["Name", "Marks"]])
+print("---------------------------------")
 print(df[["Name", "Age", "Marks"]])
+
+print("------------------------------------")
+
+print("_____________Access Rows_____________")
+print("Using iloc (index based)")
+print(df.iloc[0])
+print("df.iloc[0] print the 1st row ")
+
+print(df.iloc[0:2])
+print("df.iloc[0:2] print the 1st two  row ")

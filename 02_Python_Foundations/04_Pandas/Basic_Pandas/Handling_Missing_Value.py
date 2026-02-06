6ddb2423c515bf7940b61840ff949bd9fe3be7c9
ipartzix <parthaPaul2705@gmail.com>
2026-02-06 21:27:42 +0530

    fix(pandas):implement dropna() handling missing value note and example
    Signed-off-by: ipartzix <parthaPaul2705@gmail.com>

--- 02_Python_Foundations/04_Pandas/Basic_Pandas/Handling_Missing_Value.py
+++ 02_Python_Foundations/04_Pandas/Basic_Pandas/Handling_Missing_Value.py
@@ -8,10 +8,15 @@ data = {
 df = pd.DataFrame(data)
 print(df)
 print()
-
+print("_______________ Check missing value______________")
 print(df.isnull())
 print()
 
 print(df.isnull().sum())
 print(f"use isnull() to detect missing values and"
       f" isnull().sum() to quantify them per column before preprocessing.")
+print()
+print("_______________Remove Missing Values___________________")
+df = df.dropna()  # dropna() does NOT modify the DataFrame in place by default
+# so we create a new variable called df and store the value
+print(df)

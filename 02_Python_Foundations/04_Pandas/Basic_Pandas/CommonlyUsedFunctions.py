9f4289daafd0e4c78a3ca01a78b87fde0be5514c
ipartzix <parthaPaul2705@gmail.com>
2026-02-07 18:52:49 +0530

    feat(pandas):implement concept of df.info()
    Signed-off-by: ipartzix <parthaPaul2705@gmail.com>

--- 02_Python_Foundations/04_Pandas/Basic_Pandas/CommonlyUsedFunctions.py
+++ 02_Python_Foundations/04_Pandas/Basic_Pandas/CommonlyUsedFunctions.py
@@ -9,3 +9,6 @@ df = pd.DataFrame(data)
 print(df)
 print("____df.head(n) View top rows First n rows (data preview) count start f4rom 1 not 0____")
 print(df.head(2))
+
+print("____df.info() Data summary Data types, non-null counts, memory usage___")
+print(df.info())

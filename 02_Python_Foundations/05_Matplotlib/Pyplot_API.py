a796b90cf9ea64a1df1fac06b8276d582b30b4fd
ipartzix <parthaPaul2705@gmail.com>
2026-02-14 22:04:41 +0530

    feat(matplotlib): Add initial script for plotting salary data using Matplotlib

---
+++ 02_Python_Foundations/05_Matplotlib/Pyplot_API.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# Univariate - Numerical
+data = {
+    "Salary": [120000, 34000, 77000, 760000, 32000,54000, 670000, 45000, 13400, 29000]
+}
+
+df = pd.DataFrame(data)
+print(df.head(5))
+
+# Line plot 
+plt.plot(df["Salary"])
+plt.show()

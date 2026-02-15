ea43d80f80cb2c3d77558218a47b35087466af2e
ipartzix <parthaPaul2705@gmail.com>
2026-02-15 13:32:34 +0530

    feat(plotting): add OO API line plot for Age vs Salary with subplots

--- 02_Python_Foundations/05_Matplotlib/02_Object_Oriented_API.py
+++ 02_Python_Foundations/05_Matplotlib/02_Object_Oriented_API.py
@@ -4,10 +4,25 @@ import matplotlib.pyplot as plt
 
 from Pyplot_API import data
 print(data)
+# Create DataFrame
+df = pd.DataFrame(data)
+
+
+# Add Age column
+df["Age"] = [22, 34, 55, 66, 33, 30, 31, 26, 28, 38]
+# Sort by Age
+sort_age = df.sort_values("Age")
+
+
 # Create figure and 3 subplots (1 row, 3 columns)
 fig, axs = plt.subplots(1, 3, figsize=(10, 5))
 
-# Print objects to see what they are
-print("Figure object: - 02_Object_Oriented_API.py:11", fig)
-print("Axes objects array: - 02_Object_Oriented_API.py:12", axs)
+# Print objects
+print("Figure object: - 02_Object_Oriented_API.py:21", fig)
+print("Axes objects array: - 02_Object_Oriented_API.py:22", axs)
+
+# Example plot on first subplot
+axs[0].plot(sort_age["Age"], df["Salary"], color="red", marker="*", linewidth=2)
+axs[0].set_title("Age vs Salary")
+
 plt.show()

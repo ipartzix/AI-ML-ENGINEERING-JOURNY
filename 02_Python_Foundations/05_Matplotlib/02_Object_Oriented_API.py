55e69d5a629e14cf8eee3f7e0460b8ac8ff9705b
ipartzix <parthaPaul2705@gmail.com>
2026-02-15 13:23:52 +0530

    feat(plotting): add 3 different object-oriented plots

--- 02_Python_Foundations/05_Matplotlib/02_Object_Oriented_API.py
+++ 02_Python_Foundations/05_Matplotlib/02_Object_Oriented_API.py
@@ -4,3 +4,10 @@ import matplotlib.pyplot as plt
 
 from Pyplot_API import data
 print(data)
+# Create figure and 3 subplots (1 row, 3 columns)
+fig, axs = plt.subplots(1, 3, figsize=(10, 5))
+
+# Print objects to see what they are
+print("Figure object: - 02_Object_Oriented_API.py:11", fig)
+print("Axes objects array: - 02_Object_Oriented_API.py:12", axs)
+plt.show()

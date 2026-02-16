fdea075ff1d99c1495b415de377c6bfc077dfe60
ipartzix <parthaPaul2705@gmail.com>
2026-02-16 21:44:24 +0530

    feat(EDA):i read train dataset from csv

--- 10_Projects/02_EDA_Project/src/data_cleaning.py
+++ 10_Projects/02_EDA_Project/src/data_cleaning.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 test_data = pd.read_csv(r"D:\AI ML ENGINEERING JOURNY\10_Projects\02_EDA_Project\data\raw\test.csv")
 print(test_data.head())
+
+train_data =pd.read_csv(r"D:\AI ML ENGINEERING JOURNY\10_Projects\02_EDA_Project\data\raw\train.csv")
+print(train_data.head())

26c5c0b19b2374690536754bac90e3253200397e
ipartzix <parthaPaul2705@gmail.com>
2026-02-18 08:29:41 +0530

    remove: old EDA project files

--- 10_Projects/02_EDA_Project/src/data_cleaning.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns 
-
-train_data =pd.read_csv(r"D:\AI ML ENGINEERING JOURNY\10_Projects\02_EDA_Project\data\raw\train.csv")
-print(train_data.head())

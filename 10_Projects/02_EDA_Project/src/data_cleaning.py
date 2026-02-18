185e8ccc5066eaf851ca0c6c04ebc52960be45c3
ipartzix <parthaPaul2705@gmail.com>
2026-02-18 08:14:10 +0530

    chore(eda): Refactor the libraries

--- 10_Projects/02_EDA_Project/src/data_cleaning.py
+++ 10_Projects/02_EDA_Project/src/data_cleaning.py
@@ -1,8 +1,7 @@
 import numpy as np
-import pandas as pd 
-
-test_data = pd.read_csv(r"D:\AI ML ENGINEERING JOURNY\10_Projects\02_EDA_Project\data\raw\test.csv")
-print(test_data.head())
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
 train_data =pd.read_csv(r"D:\AI ML ENGINEERING JOURNY\10_Projects\02_EDA_Project\data\raw\train.csv")
 print(train_data.head())
